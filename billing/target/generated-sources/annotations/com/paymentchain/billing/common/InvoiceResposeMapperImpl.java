bef90792d2d32a77c6dfd9a4a0a9fb87c775c6aa
augusto1234 <augusto.cardenas@gmail.com>
2024-03-24 00:00:09 -0300

    Acctualizacion

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-03-23T23:41:39-0300",
+    date = "2024-03-23T23:59:15-0300",
     comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240206-1609, environment: Java 17.0.10 (Eclipse Adoptium)"
 )
 @Component
