fd25718a5824b01f76116d79ab806979d3731b69
augusto1234 <augusto.cardenas@gmail.com>
2024-03-23 22:35:42 -0300

    Actualizacion

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -14,8 +14,6 @@ public class BasicApplicationTests {
  public void contextLoads() {
  	    String meessage= "default message cambio test devops";
  	    Assert.assertNotNull(meessage); 
-
-		
  }
  
 }
